2823068a6845e1c7e7c1b8b230b801c5899f6af2
nysat <nysa-cere3@hotmail.com>
2023-07-29 16:20:24 -0400

    Added a new function to save items added to local storage

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+
 //   ````// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // // the code isn't run until the browser has finished rendering all the elements
 // // in the html.
@@ -8,6 +9,16 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+var scheduleItems = [];
+  
+$('.saveBtn').on('click', function() {
+  scheduleItems.push({
+    name: 'Item',
+    description: $(this).prev().val()
+  })
+  localStorage.setItem("scheduleItem", JSON.stringify(scheduleItems));
+})
+
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -18,9 +29,17 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
+
+
   //
   // TODO: Add code to display the current date in the header of the page.
   var dayStamp = moment().format('dddd' + ', ' + 'MMMM Do');
   $('#currentDay').text(dayStamp);
 });
+//  function addText(event){
+//   event.preventDefault();
+//   console.log('Text',desiredActivity.val())
+
+//  }
+// addText.on('click')
 
