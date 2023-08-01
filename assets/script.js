f47599ca19d771102f835b8802d310fdc7869fed
nysat <nysa-cere3@hotmail.com>
2023-08-01 14:47:11 -0400

    changed some of the names of ID'S in HTML

--- assets/script.js
+++ assets/script.js
@@ -25,7 +25,10 @@ $('.saveBtn').on('click', function() {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+  
+
+
+  
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
